3ad2cff203b6a2abced5eaee011ad9945ad37ab3
anjiniii <anjin720jin@naver.com>
2023-03-26 00:25:30 +0900

    [Feat] HomeViewController

--- VeganApp/Main/Home/HomeView.swift
+++ VeganApp/Main/Home/HomeView.swift
@@ -8,10 +8,109 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var showVeganInfo = true
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack{
+                Text("danto")
+                    .font(.system(size: 40, design: .rounded))
+                    .bold()
+                    .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack(alignment: .trailing) {
+                        Text("채식 시작한 지 125일 때")
+                            .padding(.bottom, 8)
+                        Text("멋쟁이 토마토")
+                        HStack {
+                            Text("레벨 1")
+                            ProgressView(value: 40, total: 100)
+                                .tint(.green)
+                                .scaleEffect(x: 1, y: 4)
+                                .frame(width: 100)
+                        }
+                    }
+                    .font(.system(size: 24, weight: .bold, design: .rounded))
+                    .padding(20)
+                }
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                HStack {
+                    Button {
+                        showVeganInfo = true
+                    } label: {
+                        RoundedRectangle(cornerRadius: 28)
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 28)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.blue)
+                }
+                .padding(20)
+                
+                RoundedRectangle(cornerRadius: 28)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.red)
+                
+                HStack {
+                    RoundedRectangle(cornerRadius: 28)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.yellow)
+                    
+                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 28)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.green)
+                }
+                .padding(20)
+            }
+            
+            if $showVeganInfo.wrappedValue {
+                ZStack {
+                            Color.black.opacity(0.4)
+                                .edgesIgnoringSafeArea(.vertical)
+                            VStack(spacing: 20) {
+                                Text("오늘의 채식 지식")
+                                    .bold().padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.orange)
+                                    .foregroundColor(Color.white)
+                                Spacer()
+                                Text("채식은 좋습니다!")
+                                    .font(.system(size: 20, weight: .bold))
+                                Button(action: {
+                                    self.showVeganInfo = false
+                                }) {
+                                    Text("Close")
+                                }.padding()
+                            }
+                            .frame(width: 300, height: 200)
+                            .background(Color.white)
+                            .cornerRadius(20).shadow(radius: 20)
+                        }
+                    }
+                
+            
+            
+        }
     }
 }
+
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
