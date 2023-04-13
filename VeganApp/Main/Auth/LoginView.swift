89e26c93e878ad30ac50b87e251ba3e1deab4e86
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-04-13 19:00:23 +0900

    [Feat] SignUpView UI 구현

--- VeganApp/Main/Auth/LoginView.swift
+++ VeganApp/Main/Auth/LoginView.swift
@@ -8,79 +8,91 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State private var id = ""
+    @State private var userId = ""
     @State private var password = ""
     
     @FocusState private var textFieldIsFocused: Bool
     
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    Image("homeLogoImg")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200)
-                    Text("조금 느리게, 너만의 토마토를 키워봐")
-                        .padding(.bottom, 20)
-                }
-                Spacer()
-            }
-            
-            TextField("아이디를 입력해주세요", text: $id)
-                .textInputAutocapitalization(.never)
-                .padding()
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.gray, lineWidth: 1)
-                )
-                .focused($textFieldIsFocused)
-            
-            SecureField("비밀번호를 입력해주세요", text: $password)
-                .textInputAutocapitalization(.never)
-                .padding()
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.gray, lineWidth: 1)
-                )
-                .focused($textFieldIsFocused)
-            
-            Button {
+        NavigationView {
+            VStack {
+                logoView
+                userIdTextField
+                passwordSecureField
                 
-            } label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 12)
-                        .frame(height: 48)
-                    
-                    Text("로그인")
-                        .font(.system(size: 20))
-                        .fontWeight(.bold)
-                        .foregroundColor(.black)
+                Button {
+                    // TODO: login
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .frame(height: 48)
+                        
+                        Text("로그인")
+                            .font(.system(size: 20))
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                    }
+                    .padding(.vertical)
                 }
-                .padding(.vertical)
-            }
-            
-            Button {
                 
-            } label: {
-                HStack {
-                    Text("처음 오셨나요?")
-                    
-                    Text("회원가입하기")
-                    Image(systemName: "chevron.right")
+                NavigationLink {
+                    SignUpView()
+                } label: {
+                    HStack {
+                        Text("처음 오셨나요?")
+                        
+                        Text("회원가입하기")
+                        Image(systemName: "chevron.right")
+                    }
+                    .foregroundColor(.gray)
                 }
-                .foregroundColor(.gray)
             }
-        }
-        .padding(.horizontal, 30)
-        .toolbar {
-            ToolbarItemGroup(placement: .keyboard) {
-                Spacer()
-                Button("완료") {
-                    textFieldIsFocused = false
+            .padding(.horizontal, 30)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("완료") {
+                        textFieldIsFocused = false
+                    }
                 }
             }
         }
+    }
+    
+    private var logoView: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Image("homeLogoImg")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200)
+                Text("조금 느리게, 너만의 토마토를 키워봐")
+                    .padding(.bottom, 20)
+            }
+            Spacer()
+        }
+    }
+    
+    private var userIdTextField: some View {
+        TextField("아이디를 입력해주세요", text: $userId)
+            .textInputAutocapitalization(.never)
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+            .focused($textFieldIsFocused)
+    }
+    
+    private var passwordSecureField: some View {
+        SecureField("비밀번호를 입력해주세요", text: $password)
+            .textInputAutocapitalization(.never)
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+            .focused($textFieldIsFocused)
     }
 }
 
---
+++ VeganApp/Main/Auth/SignUpView.swift
@@ -0,0 +1,131 @@
+//
+//  SignUpView.swift
+//  VeganApp
+//
+//  Created by 이안진 on 2023/04/13.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    @State private var userId = ""
+    @State private var nickName = ""
+    @State private var password = ""
+    @State private var verifyPassword = ""
+    @State private var verifyPasswordMessage = ""
+    
+    @State private var isRightPassword = false
+    @FocusState private var textFieldIsFocused: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            userIdTextField
+            nickNameTextField
+            passwordSecureField
+            verifyPasswordSecureField
+            
+            Button {
+                // TODO: signUp
+                if verifyPassword == password {
+                    isRightPassword = true
+                    verifyPasswordMessage = "비밀번호가 확인되었습니다"
+                } else {
+                    isRightPassword = false
+                    verifyPasswordMessage = "비밀번호가 다릅니다!"
+                }
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .frame(height: 48)
+                    
+                    Text("회원가입")
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                }
+                .padding(.vertical)
+            }
+        }
+        .padding(.horizontal, 30)
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                Button("완료") {
+                    textFieldIsFocused = false
+                }
+            }
+        }
+        .navigationTitle("회원가입")
+    }
+    
+    var userIdTextField: some View {
+        VStack(alignment: .leading) {
+            Text("아이디")
+                .fontWeight(.bold)
+            TextField("아이디를 입력해주세요", text: $userId)
+                .textInputAutocapitalization(.never)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .focused($textFieldIsFocused)
+        }
+        .padding(.bottom)
+    }
+    
+    var nickNameTextField: some View {
+        VStack(alignment: .leading) {
+            Text("닉네임")
+                .fontWeight(.bold)
+            TextField("닉네임을 입력해주세요", text: $nickName)
+                .textInputAutocapitalization(.never)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .focused($textFieldIsFocused)
+        }
+        .padding(.bottom)
+    }
+    
+    var passwordSecureField: some View {
+        VStack(alignment: .leading) {
+            Text("비밀번호")
+                .fontWeight(.bold)
+            SecureField("비밀번호를 입력해주세요", text: $password)
+                .textInputAutocapitalization(.never)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .focused($textFieldIsFocused)
+        }
+        .padding(.bottom)
+    }
+    
+    var verifyPasswordSecureField: some View {
+        VStack(alignment: .leading) {
+            Text("비밀번호 확인")
+                .fontWeight(.bold)
+            SecureField("비밀번호를 한 번 더 입력해주세요", text: $verifyPassword)
+                .textInputAutocapitalization(.never)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .focused($textFieldIsFocused)
+            Text(verifyPasswordMessage)
+        }
+        .padding(.bottom)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
